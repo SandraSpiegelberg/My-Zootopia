9a7c7e9d3655ab94aecbc036d0abdc6ebac56c48
Sandras Mac <sandra@46:f9:3b:93:80:fc.speedport.ip>
2025-06-20 13:57:09 +0200

    add animals.html and create function that convert the selected information from the list into a string and returns the string

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -2,6 +2,7 @@ import json
 
 def load_data(file_path):
   """ Loads a JSON file
+
     :param file_path: Path to the JSON file
     :return: loaded JSON file
   """
@@ -15,6 +16,7 @@ def select_animal_info(data):
   """
   selects the information {Name, Diet, first location, Type}
   if one of these fields doesn't exist, then it doesn't exist as well in the dictionary
+
   :param data: loaded data
   :return:  list of dictionaries with selected information
   """
@@ -22,22 +24,35 @@ def select_animal_info(data):
   for animal in data:
       info_dict = {}
       if 'name' in animal:
-        print('Name:', animal['name'])
-        info_dict['Name'] = animal['name']
+          #print('Name:', animal['name'])
+          info_dict['Name'] = animal['name']
       if 'diet' in animal['characteristics']:
-        print('Diet:', animal['characteristics']['diet'])
-        info_dict['Diet'] = animal['characteristics']['diet']
+          #print('Diet:', animal['characteristics']['diet'])
+          info_dict['Diet'] = animal['characteristics']['diet']
       if len(animal['locations']) > 0:
-        print('First Location:', animal['locations'][0])
-        info_dict['First Location'] = animal['locations'][0]
+          #print('First Location:', animal['locations'][0])
+          info_dict['First Location'] = animal['locations'][0]
       if 'type' in animal['characteristics']:
-        print('Type:', animal['characteristics']['type'])
-        info_dict['Type'] = animal['characteristics']['type']
+          #print('Type:', animal['characteristics']['type'])
+          info_dict['Type'] = animal['characteristics']['type']
       animals_list.append(info_dict)
-  print(animals_list)
+  #print(animals_list)
   return animals_list
 
+def create_str_of_info(list_of_dict):
+  """
+  create a string that include all the selected informations and line breaks
+  :param list_of_dict: list of dictionaries with selected information
+  :return:  string with whole selected information
+  """
+  animals_str = ''
+  for animal in list_of_dict:
+     for key, value in animal.items():
+        animals_str += key + ' ' + value + '\n'
+  print(animals_str)
 
-select_animal_information(animals_data)
+list_infos = select_animal_info(animals_data)
+create_str_of_info(list_infos)
+
 
 
